60e434911991754c5c92cb11d074995fa0b4fbd8
ROHITH05012005 <contact@fuera.in.net>
2026-09-02 23:51:43 +0530

    feat: configure Capacitor native Android application platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.optiload.app',
+  appName: 'OptiLoad 3D',
+  webDir: 'dist'
+};
+
+export default config;
